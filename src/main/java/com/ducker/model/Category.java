94c66169a8693a5250efe199718b5f484650591f
MaksimMartsiusheu <mmartsiusheu@gmail.com>
2020-05-14 14:06:10 +0300

    Init commit

---
+++ src/main/java/com/ducker/model/Category.java
@@ -0,0 +1,28 @@
+package com.ducker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @NotBlank(message = "Name may not be empty")
+    @Column(name = "category_name")
+    private String name;
+
+}
